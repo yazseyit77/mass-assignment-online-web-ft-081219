250a601ccf1ac13b9e2fb687a154453599b96a54
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 17:28:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -23,3 +23,23 @@ class Person
     # attributes.each{|key, value| self..send(("#{key}="), value)}
   end
 end
+
+
+class Person
+  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed
+
+
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:,
+    weight:, handed:)
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+      # attributes.each{|key, value| self..send(("#{key}="), value)}
+  end
+end
