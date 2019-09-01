31f93b859d2e09f9f132e6608cfde7c4a8f18aee
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 17:57:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,33 +5,35 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def initialize(name:, birthday:, hair_color:, eye_color:, height:,
-    weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
-    @name = name
-    @birthday = birthday
-    @hair_color = hair_color
-    @eye_color = eye_color
-    @height = height
-    @weight = weight
-    @handed = handed
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size
-    @glove_size = glove_size
-    @pant_length = pant_length
-    @pant_width = pant_width
-    # attributes.each{|key, value| self..send(("#{key}="), value)}
-  end
-end
-
-
-class Person
-  #your code here
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-    :weight, :handed
-
-
-  def initialize(attributes)
+  def initialize()
+    # @name = name
+    # @birthday = birthday
+    # @hair_color = hair_color
+    # @eye_color = eye_color
+    # @height = height
+    # @weight = weight
+    # @handed = handed
+    # @complexion = complexion
+    # @t_shirt_size = t_shirt_size
+    # @wrist_size = wrist_size
+    # @glove_size = glove_size
+    # @pant_length = pant_length
+    # @pant_width = pant_width
     attributes.each{|key, value| self..send(("#{key}="), value)}
   end
 end
+
+
+# name:, birthday:, hair_color:, eye_color:, height:,
+#   weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:
+
+# class Person
+#   #your code here
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+#     :weight, :handed
+# 
+# 
+#   def initialize(attributes)
+#     attributes.each{|key, value| self..send(("#{key}="), value)}
+#   end
+# end
