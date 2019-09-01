4ccf7d02e590ca73d88b1adbb0748f7dbdcbc25f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 18:03:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
     :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:)
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
     @name = name
     @birthday = birthday
     @hair_color = hair_color
@@ -15,10 +15,10 @@ class Person
     @handed = handed
     @complexion = complexion
     @t_shirt_size = t_shirt_size
-    # @wrist_size = wrist_size
-    # @glove_size = glove_size
-    # @pant_length = pant_length
-    # @pant_width = pant_width
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = pant_width
     # attributes.each {|key, value| self..send(("#{key}="), value)}
   end
 end
