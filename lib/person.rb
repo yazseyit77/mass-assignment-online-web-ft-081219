179da338c896927550bb749931b36c7497499693
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 18:01:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def initialize(attributes)
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, attributes)
     # @name = name
     # @birthday = birthday
     # @hair_color = hair_color
