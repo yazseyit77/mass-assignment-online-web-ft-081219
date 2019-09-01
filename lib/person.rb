db0e96af86dbe42e86610075bf8ff37a5c1d0a60
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 17:24:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,6 +6,13 @@ class Person
 
   def initialize(name:, birthday:, hair_color:, eye_color:, height:,
     weight:, handed:)
-    attributes.each{|key, value| self..send(("#{key}="), value)}
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    # attributes.each{|key, value| self..send(("#{key}="), value)}
   end
 end
